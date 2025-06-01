21553088c170bb204a8057ce1970405b696d8cbd
LinerUP <gjonakimaser@gmail.com>
2025-06-01 21:35:44 +0200

    bot link

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,7 @@ TOKEN = "7916867890:AAF1e2EJ0jPXc7fOwUE3v0EvzhavgvSXW-Q"
 
 async def start(update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("Открыть Web App", web_app=WebAppInfo(url="https://telegram-mvcu97syc-linerups-projects.vercel.app/"))]
+        [InlineKeyboardButton("Открыть Web App", web_app=WebAppInfo(url="https://telegram-bot-7np8.vercel.app/"))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Привет! Нажми кнопку, чтобы открыть Web App:", reply_markup=reply_markup)
